2adcf6e888a8f40f72351d1565d9417a0c6ad5e0
wind2sing <windsing.me@gmail.com>
2020-10-06 21:50:05 +0800

    use parse-parsed;support parseCheck

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const got = require("got").default;
-const { loadCheerio, parse } = require("cparse");
+const { loadCheerio, parse: parseIt } = require("cparse");
 const debugHttp = require("./debug-http")();
 function create({ filters = {}, cheerioOptions = {} } = {}) {
   const instance = got.extend(debugHttp, {
@@ -19,8 +19,14 @@ function create({ filters = {}, cheerioOptions = {} } = {}) {
         },
         function parseRes(res) {
           if (res.$) {
-            const { rule } = res.request.options;
-            if (rule) res.item = parse(rule, res.$, filters);
+            let { parse, parseCheck } = res.request.options;
+            if (parseCheck) {
+              const checkResult = parseCheck(res);
+              if (!checkResult) return;
+              if (["object", "string", "function"].includes(typeof checkResult))
+                parse = checkResult;
+            }
+            if (parse) res.parsed = parseIt(rule, res.$, filters);
           }
           return res;
         },
